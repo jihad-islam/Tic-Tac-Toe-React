29745791ad77e40d293cc89fb4a6d4f951cc1ad1
jihad-islam <jihadislam.diu@gmail.com>
2025-07-22 23:27:53 +0600

    first commit

---
+++ src/components/Board.jsx
@@ -0,0 +1,45 @@
+/*
+    square and board duitai component. components are nothing else but a function. but board holo default component.
+*/
+
+function Square({ value }) {
+  function handleClick() {
+    console.log(value);
+  }
+
+  return (
+    <button
+      className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg"
+      //   onClick={() => console.log(value)}
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+function Board() {
+  return (
+    <>
+      <div>
+        <Square value={1} />
+        <Square value={2} />
+        <Square value={3} />
+      </div>
+
+      <div>
+        <Square value={4} />
+        <Square value={5} />
+        <Square value={6} />
+      </div>
+
+      <div>
+        <Square value={7} />
+        <Square value={8} />
+        <Square value={9} />
+      </div>
+    </>
+  );
+}
+
+export default Board;
---
+++ src/components/Hitory.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Hitory() {
+  return (
+    <div>Hitory</div>
+  )
+}
+
+export default Hitory
