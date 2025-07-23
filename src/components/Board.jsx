81d5fa6afd00e4c4eade92287a1ca97bfeec0421
jihad-islam <jihadislam.diu@gmail.com>
2025-07-24 00:18:58 +0600

    v3 done

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,6 +1,9 @@
 /*
     1.version 3: ekhane lifting the state up kora hobe.
     2. version 2 te Square component e state chilo and Square component theke state er maddhome amra square e ekta static value assign korechilam. but amra condition er maddhome value assign korte chai. first e 'X' thakle erpor 'O' hobe. so amake track rakhte hobe aager square e value ki chilo. ei track ta Square component rekhe lav nai, track rakha lagbe Square component er parent Board component e. tai Square component theke state ta Board component e lifting up korte hobe. 
+
+    3. bojhar shubidarthe only ekta square niye kaj korsi.
+    4. for concept see src/components/concept.md 
 */
 import { useState } from "react";
 
@@ -16,12 +19,12 @@ function Square({ value, onSquareClick }) {
 }
 
 function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [squares, setSquares] = useState(Array(9).fill(null)); // state hishebe array newa hoise
 
   function handleClick() {
     squares[0] = "X";
-    // setSquares(squares);
-    setSquares([...squares]);
+    // setSquares(squares); // state update korle react re-render hoy.state hishebe array or object niye kaj korle ektu jhamela hoy. amra jokhon array or object er kono value change or update kori, tokhon react render kore aager array and changed array compare kore dekhe. same array or object diye state update korle react bujhte pare na je state change hoyeche. so react render ow kore na.
+    setSquares([...squares]); // spread operator er maddhome notun array create kore setSquares diye state update kora hoyeche.ekhn react re-render er shomoy state er change identify korte parbe.
   }
 
   return (
